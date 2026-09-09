aceab07e25d9096583d21d53b4f079b705926d99
Aman Bhargava <amanbhargava2001@gmail.com>
2026-09-09 16:10:03 +0530

    fix: drop at:// link tags from layout (breaks prerender crawler)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,8 +11,5 @@
 	<link rel="apple-touch-icon" href="{base}/apple-touch-icon.png" />
 	<link rel="manifest" href="{base}/site.webmanifest" />
 	<meta name="theme-color" content="#0B1D3A" />
-	<!-- Standard.site: this project is a document of the Diagram Chasing publication -->
-	<link rel="site.standard.publication" href="at://did:plc:pbzkofwfyokqzouibm2bcfo4/site.standard.publication/3mv3cvvqd72hd" />
-	<link rel="site.standard.document" href="at://did:plc:pbzkofwfyokqzouibm2bcfo4/site.standard.document/3mv3cvxrsvkhb" />
 </svelte:head>
 {@render children()}
